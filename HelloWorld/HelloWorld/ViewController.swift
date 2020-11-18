12aed739a3ab2765ea17bf4e0d62479ac66dbb05
Mohammed Habib Mansoor Habib Nasser <201500350@student.polytechnic.bh>
2020-11-18 16:44:26 +0300

    change 13

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //test change 12
+        //test change 13
     }
 
 
